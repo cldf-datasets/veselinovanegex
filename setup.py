94801d4fae81a959a8f484f5fccbbbf1d4197143
Johannes Englisch <johannes_englisch@eva.mpg.de>
2024-08-27 12:27:10 +0200

    set up pyproject.toml

--- setup.py
+++ setup.py
@@ -1,22 +1,3 @@
 from setuptools import setup
 
-
-setup(
-    name='cldfbench_veselinovanegex',
-    py_modules=['cldfbench_veselinovanegex'],
-    include_package_data=True,
-    zip_safe=False,
-    entry_points={
-        'cldfbench.dataset': [
-            'veselinovanegex=cldfbench_veselinovanegex:Dataset',
-        ]
-    },
-    install_requires=[
-        'cldfbench[glottolog,excel]',
-    ],
-    extras_require={
-        'test': [
-            'pytest-cldf',
-        ],
-    },
-)
+setup()
