2dab50f225f8eb866cbd57b746cab536ac0ec1fd
Johannes Englisch <johannes_englisch@eva.mpg.de>
2024-08-29 11:23:21 +0200

    Adapted code to changed data schema

--- cldfbench_veselinovanegex.py
+++ cldfbench_veselinovanegex.py
@@ -1,55 +1,13 @@
 import re
 import pathlib
+from itertools import islice
 
 from pybtex import errors
 from pycldf.sources import Sources
 from cldfbench import Dataset as BaseDataset, CLDFSpec
 
 
-def citation(src):
-    name, year = None, src.get('year')
-    if 'author' in src:
-        name = ' & '.join(
-            s.split(',')[0]
-            for s in src['author'].split(' and '))
-    return '{} {}'.format(name, year)
-
-
-def parse_citation(text):
-    if not text:
-        return None, None
-    if ':' in text:
-        ref, pages = text.split(':', maxsplit=1)
-    else:
-        ref, pages = text, None
-    ref = ref.replace('(', '').replace(',', '') \
-        .replace('MIchael', 'Michael') \
-        .replace('MIestam', 'Miestam') \
-        .replace('Brandup', 'Brandrup')
-    ref = re.sub(r'\s+', ' ', ref)
-    ref = ref.replace(' and ', ' & ')
-    if 'p.c.' not in ref.replace(' ', ''):
-        return ref, pages.strip() if pages else pages
-    return None, None
-
-
-class Dataset(BaseDataset):
-    dir = pathlib.Path(__file__).parent
-    id = "veselinovanegex"
-
-    def cldf_specs(self):  # A dataset must declare all CLDF sets it creates.
-        return CLDFSpec(module='StructureDataset', dir=self.cldf_dir)
-
-    def cmd_download(self, args):
-        pass
-
-    def cmd_makecldf(self, args):
-        errors.strict = False
-        sources = {}
-        for src in Sources.from_file(self.raw_dir / 'NegEx_bib.txt'):
-            sources[citation(src)] = src
-        args.writer.cldf.add_sources(*list(sources.values()))
-        args.writer.cldf.add_sources("""
+VESELINOVA_2013 = """\
 @article{veselinova2013,
     author = {Ljuba Veselinova},
     year = {2013},
@@ -58,74 +16,168 @@ class Dataset(BaseDataset):
     issue = {1},
     pages = {107-145}
 }
-""")
-        args.writer.cldf.add_component('LanguageTable')
-        args.writer.cldf.add_component(
-            'ParameterTable',
-            {
-                'name': 'Grammacodes',
-                'separator': ';',
-            })
-        args.writer.cldf.add_component('CodeTable', 'Map_Icon')
+"""
 
-        liso2gl = {
+
+def read_fixed_isos(csv_assocs):
+    assert csv_assocs[0] == ['NAM_LABEL', 'ID_ISO_A3']
+    return {
+        label: isocode
+        for label, isocode in islice(csv_assocs, 1, None)}
+
+
+def make_languages(data, fixed_isos, languoids_by_iso):
+    return [
+        {
+            'ID': (iso := fixed_isos.get(row['NAM_LABEL']) or row['ID_ISO_A3']),
+            'Name': row['NAM_LABEL'],
+            'ISO639P3code': iso,
+            'Glottocode': glang.id if (glang := languoids_by_iso.get(iso)) else None,
+            'Latitude': glang.latitude if glang else None,
+            'Longitude': glang.longitude if glang else None,
+        }
+        for row in data]
+
+
+def make_parameters(csv_parameters):
+    parameters = {
+        parameter['Original_Name']: parameter
+        for parameter in csv_parameters}
+    for parameter in parameters.values():
+        if (grammacodes := parameter.get('Grammacodes')):
+            parameter['Grammacodes'] = [
+                id_.strip()
+                for id_ in grammacodes.split(',')]
+    return parameters
+
+
+def make_codes(csv_codes):
+    return {
+        (row['Parameter_ID'], row['Original_Name']): row
+        for row in csv_codes}
+
+
+def citation_from_bibtex(bibtex_entry):
+    if (authors := bibtex_entry.get('author')):
+        names = ' & '.join(a.split(',')[0] for a in authors.split(' and '))
+    else:
+        names = None
+    year = bibtex_entry.get('year')
+    return f'{names} {year}'
+
+
+def make_sources(bibtex_sources):
+    return {citation_from_bibtex(entry): entry for entry in bibtex_sources}
+
+
+def citation_from_authoryear(text):
+    if not text:
+        return None, None
+
+    if ':' in text:
+        citation, pages = text.split(':', maxsplit=1)
+    else:
+        citation, pages = text, None
+
+    citation = citation.replace('(', '')
+    citation = citation.replace(',', '')
+    citation = citation.replace('MIchael', 'Michael')
+    citation = citation.replace('MIestam', 'Miestam')
+    citation = citation.replace('Brandup', 'Brandrup')
+    citation = re.sub(r'\s+', ' ', citation)
+    citation = citation.replace(' and ', ' & ')
+
+    if 'p.c.' not in citation.replace(' ', ''):
+        return citation, pages.strip() if pages else pages
+    else:
+        return None, None
+
+
+def make_value(
+    row, param_col, params_by_col, codes_by_value, sources_by_citation
+):
+    citations = []
+    if param_col == 'SN':
+        citation, pages = citation_from_authoryear(row['Source_SN'])
+        if citation in sources_by_citation:
+            bibkey = sources_by_citation[citation].id
+            citations = [f'{bibkey}[{pages}]' if pages else bibkey]
+    elif param_col == 'NegEx_Form':
+        citation, pages = citation_from_authoryear(row['Source_NegEx'])
+        if citation in sources_by_citation:
+            bibkey = sources_by_citation[citation].id
+            citations = [f'{bibkey}[{pages}]' if pages else bibkey]
+    language_id = row['ID_ISO_A3']
+    original_value = row[param_col].strip()
+    parameter = params_by_col[param_col]
+    parameter_id = parameter['ID']
+    code = codes_by_value.get((parameter_id, original_value))
+    return {
+        'ID': f'{language_id}-{param_col}',
+        'Code_ID': code['ID'] if code else '',
+        'Value': code['Name'] if code else original_value,
+        'Language_ID': language_id,
+        'Parameter_ID': parameter_id,
+        'Source': citations,
+    }
+
+
+def make_values(data, params_by_col, codes_by_value, sources_by_citation):
+    parameter_columns = ['SN', 'NegEx_Form', 'NegExType_general_classificaiton']
+    return [
+        make_value(row, param_col, params_by_col, codes_by_value, sources_by_citation)
+        for row in data
+        for param_col in parameter_columns]
+
+
+def update_cldf_schema(cldf):
+    cldf.add_component('LanguageTable')
+    cldf.add_component(
+        'ParameterTable',
+        {
+            'name': 'Grammacodes',
+            'separator': ';',
+        })
+    cldf.add_component('CodeTable', 'Map_Icon')
+
+
+class Dataset(BaseDataset):
+    dir = pathlib.Path(__file__).parent
+    id = "veselinovanegex"
+
+    def cldf_specs(self):
+        # A dataset must declare all CLDF sets it creates.
+        return CLDFSpec(module='StructureDataset', dir=self.cldf_dir)
+
+    def cmd_download(self, args):
+        pass
+
+    def cmd_makecldf(self, args):
+        raw_data = list(self.raw_dir.read_csv('NegEx_CLDF.csv', dicts=True))
+
+        languoids_by_iso = {
             languoid.iso: languoid
-            for languoid in args.glottolog.api.languoids() if languoid.iso}
-        language_errata = {
-            r['NAM_LABEL']: r
-            for r in self.etc_dir.read_csv('languages.csv', dicts=True)}
-        parameters = list(
+            for languoid in args.glottolog.api.languoids()
+            if languoid.iso}
+        fixed_isos = read_fixed_isos(self.etc_dir.read_csv('languages.csv'))
+        languages = make_languages(raw_data, fixed_isos, languoids_by_iso)
+
+        params_by_col = make_parameters(
             self.etc_dir.read_csv('parameters.csv', dicts=True))
-        codes = {
-            (row['Parameter_ID'], row['Original_Name']): row
-            for row in self.etc_dir.read_csv('codes.csv', dicts=True)}
-        for parameter in parameters:
-            if (grammacodes := parameter.get('Grammacodes')):
-                parameter['Grammacodes'] = [
-                    id_.strip()
-                    for id_ in grammacodes.split(',')]
-        args.writer.objects['ParameterTable'] = parameters
-        args.writer.objects['CodeTable'] = codes.values()
-        for row in self.raw_dir.read_csv('NegEx_CLDF.csv', dicts=True):
-            if row['NAM_LABEL'] in language_errata:
-                row.update(language_errata[row['NAM_LABEL']])
-            lid = row['ID_ISO_A3']
-            glang = liso2gl.get(lid)
-            args.writer.objects['LanguageTable'].append(dict(
-                ID=lid,
-                Name=row['NAM_LABEL'],
-                ISO639P3code=lid,
-                Glottocode=glang.id if glang else None,
-                Latitude=glang.latitude if glang else None,
-                Longitude=glang.longitude if glang else None,
-            ))
-            for pid in ['SN', 'NegEx_Form', 'NegExType']:
-                refs = []
-                if pid == 'SN':
-                    ref, pages = parse_citation(row['Source_SN'])
-                    if ref in sources:
-                        key = sources[ref].id
-                        if pages:
-                            key += '[{}]'.format(pages)
-                        refs.append(key)
-                elif pid == 'NegEx_Form':
-                    ref, pages = parse_citation(row['Source_NegEx'])
-                    if ref in sources:
-                        key = sources[ref].id
-                        if pages:
-                            key += '[{}]'.format(pages)
-                        refs.append(key)
-                else:
-                    refs = []
-                original_value = row[pid]
-                code = codes.get((pid, original_value))
-                code_id = code['ID'] if code else ''
-                value = code['Name'] if code else original_value
-                args.writer.objects['ValueTable'].append(dict(
-                    ID='{}-{}'.format(lid, pid),
-                    Code_ID=code_id,
-                    Value=value,
-                    Language_ID=lid,
-                    Parameter_ID=pid,
-                    Source=refs,
-                ))
+        codes_by_value = make_codes(self.etc_dir.read_csv('codes.csv', dicts=True))
+
+        errors.strict = False
+        sources_by_citation = make_sources(Sources.from_file(self.raw_dir / 'NegEx_bib.txt'))
+
+        values = make_values(
+            raw_data, params_by_col, codes_by_value, sources_by_citation)
+
+        update_cldf_schema(args.writer.cldf)
+
+        args.writer.objects['LanguageTable'] = languages
+        args.writer.objects['ParameterTable'] = params_by_col.values()
+        args.writer.objects['CodeTable'] = codes_by_value.values()
+        args.writer.objects['ValueTable'] = values
+
+        args.writer.cldf.add_sources(*sources_by_citation.values())
+        args.writer.cldf.add_sources(VESELINOVA_2013)
